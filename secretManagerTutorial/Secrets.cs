be5920ec1cf66686b4d30a8fde9e38617ad5977b
mitchellm <developer@mitchellmudd.dev>
2023-09-21 18:18:35 -0400

    Initial commit :)

---
+++ secretManagerTutorial/Secrets.cs
@@ -0,0 +1,16 @@
+namespace secretManagerTutorial;
+
+public class Secrets
+{
+    public static string FetchSecret()
+    {
+        // Setup the config object
+        var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+        
+        // Access the ServiceApiKey secret
+        var clientId = config["ServiceApiKey"];
+        
+        // clientId could be null since we aren't sure what's in the secret manager. Make sure to check :)
+        return clientId == null ? "" : clientId;
+    }
+}
